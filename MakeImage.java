47d8d921797d3b2f8729fc698f82c40f4b2bfe5e
Penn Wu <penn.z.wu@gmail.com>
2018-02-02 11:48:55 -0500

    WIP

---
+++ MakeImage.java
@@ -0,0 +1,7 @@
+import java.io.FileWriter;
+public class MakeImage {
+    public static void main(String[] args) {
+	// For now, generate an 800x800 image. Maybe let the user specify dimensions later?
+	
+    }
+}
