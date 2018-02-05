517112732f10f31e8d104f7528c62e9ceb46fd7c
Penn Wu <penn.z.wu@gmail.com>
2018-02-05 01:06:27 -0500

    Delete MakeImage.java

--- MakeImage.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileWriter;
-public class MakeImage {
-    public static void main(String[] args) {
-	// For now, generate an 800x800 image. Maybe let the user specify dimensions later?
-	
-    }
-}
